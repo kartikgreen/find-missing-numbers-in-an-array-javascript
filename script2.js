bc8ab4b84c7300d1ecae338591a63d52e3451100
ngmicroapp <ngmicroapp@gmail.com>
2021-07-09 11:39:43 +0530

    supports negative number

--- script2.js
+++ script2.js
@@ -1,23 +1,23 @@
 function getAllMissingNumbers(item) {
-    let first = 0;
-    let second = 1;
-    let currentValue = item[0];
-    const container = [];
-    while (first < second && item[second]) {
-      if ((item[first] + 1) !== item[second]) {
-        if ((currentValue + 1) === item[second]) {
-          first = second;
-          second++;
-          currentValue = item[first];
-        } else {
-          container.push(++currentValue);
-        }
-      } else {
+  let first = 0;
+  let second = 1;
+  let currentValue = item[0];
+  const container = [];
+  while (first < second && item[second]) {
+    if ((item[first] + 1) !== item[second]) {
+      if ((currentValue + 1) === item[second]) {
         first = second;
         second++;
         currentValue = item[first];
+      } else {
+        container.push(++currentValue);
       }
+    } else {
+      first = second;
+      second++;
+      currentValue = item[first];
     }
-    return container;
   }
-  console.log(getAllMissingNumbers([-2,1,2,8,9,10,14]));
+  return container;
+}
+console.log(getAllMissingNumbers([-2,1,2,8,9,10,14]));
